6f9faffa582d504a6a378c89f4e32c71714c6e97
iam91 <zhengwy91@foxmail.com>
2018-04-08 23:27:13 +0800

    update

--- src/tags/bfs/SurroundedRegions.java
+++ src/tags/bfs/SurroundedRegions.java
@@ -1,9 +1,11 @@
 package tags.bfs;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SurroundedRegions {
+    // https://leetcode.com/problems/surrounded-regions/description/
+
     public void solve(char[][] board) {
 
         if(board == null || board.length == 0 || board[0].length == 0) return;
@@ -11,62 +13,54 @@ public class SurroundedRegions {
         int n = board.length;
         int m = board[0].length;
 
-        boolean[][] visited = new boolean[n][m];
+        for(int i = 0; i < n; i++) {
+            bfs(board, i, 0, n, m);
+            bfs(board, i, m - 1, n, m);
+        }
+
+        for(int i = 0; i < m; i++) {
+            bfs(board, 0, i, n, m);
+            bfs(board, n - 1, i, n, m);
+        }
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(board[i][j] == 'O' && !visited[i][j]) {
-                    //bfs
-                    bfs(board, visited, i, j, n, m);
-                }
+                board[i][j] = board[i][j] == '+' ? 'O' : 'X';
             }
         }
     }
 
-    private void bfs(char[][] board, boolean[][] visited, int i, int j, int n, int m) {
+    private void bfs(char[][] board, int i, int j, int n, int m) {
 
-        Queue<Integer> xq = new LinkedList<>();
-        Queue<Integer> yq = new LinkedList<>();
+        if(board[i][j] == 'X') return;
 
-        LinkedList<Integer> xv = new LinkedList<>();
-        LinkedList<Integer> yv = new LinkedList<>();
+        List<Integer> xq = new ArrayList<>();
+        List<Integer> yq = new ArrayList<>();
 
-        xq.offer(i);
-        yq.offer(j);
-
-        boolean flag = false;
+        xq.add(i);
+        yq.add(j);
 
         while(!xq.isEmpty()) {
-            int x = xq.poll();
-            int y = yq.poll();
+            int x = xq.remove(0);
+            int y = yq.remove(0);
 
-            xv.offer(x);
-            yv.offer(y);
+            board[x][y] = '+';
 
-            visited[x][y] = true;
-            if(x <= 0 || y <= 0 || x >= n - 1 || y >= m - 1) flag = true;
-
-            if(x > 0 && !visited[x - 1][y] && board[x - 1][y] == 'O') {
-                xq.offer(x - 1);
-                yq.offer(y);
+            if(x > 0 && board[x - 1][y] == 'O') {
+                xq.add(x - 1);
+                yq.add(y);
             }
-            if(x < n - 1 && !visited[x + 1][y] && board[x + 1][y] == 'O') {
-                xq.offer(x + 1);
-                yq.offer(y);
+            if(x < n - 1 && board[x + 1][y] == 'O') {
+                xq.add(x + 1);
+                yq.add(y);
             }
-            if(y > 0 && !visited[x][y - 1] && board[x][y - 1] == 'O') {
-                xq.offer(x);
-                yq.offer(y - 1);
+            if(y > 0 && board[x][y - 1] == 'O') {
+                xq.add(x);
+                yq.add(y - 1);
             }
-            if(y < m - 1 && visited[x][y + 1] && board[x][y + 1] == 'O') {
-                xq.offer(x);
-                yq.offer(y + 1);
-            }
-        }
-
-        if(!flag) {
-            for(int ii = 0; ii < xv.size(); ii++) {
-                board[xv.get(ii)][yv.get(ii)] = 'X';
+            if(y < m - 1 && board[x][y + 1] == 'O') {
+                xq.add(x);
+                yq.add(y + 1);
             }
         }
     }
@@ -74,14 +68,20 @@ public class SurroundedRegions {
     public static void main(String[] args) {
         SurroundedRegions c = new SurroundedRegions();
 
+//        char[][] board = {
+//                {'O', 'O', 'O', 'O', 'X', 'X'},
+//                {'O', 'O', 'O', 'O', 'O', 'O'},
+//                {'O', 'X', 'O', 'X', 'O', 'O'},
+//                {'O', 'X', 'O', 'O', 'X', 'O'},
+//                {'O', 'X', 'O', 'X', 'O', 'O'},
+//                {'O', 'X', 'O', 'O', 'O', 'O'},
+//
+//        };
         char[][] board = {
-                {'O', 'O', 'O', 'O', 'X', 'X'},
-                {'O', 'O', 'O', 'O', 'O', 'O'},
-                {'O', 'X', 'O', 'X', 'O', 'O'},
-                {'O', 'X', 'O', 'O', 'X', 'O'},
-                {'O', 'X', 'O', 'X', 'O', 'O'},
-                {'O', 'X', 'O', 'O', 'O', 'O'},
-
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'X', 'X'},
         };
 
         for(int i = 0; i < board.length; i++) {
---
+++ src/tags/bfs/SurroundedRegionsMy.java
@@ -0,0 +1,105 @@
+package tags.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SurroundedRegionsMy {
+    public void solve(char[][] board) {
+
+        if(board == null || board.length == 0 || board[0].length == 0) return;
+
+        int n = board.length;
+        int m = board[0].length;
+
+        boolean[][] visited = new boolean[n][m];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] == 'O' && !visited[i][j]) {
+                    bfs(board, visited, i, j, n, m);
+                }
+            }
+        }
+    }
+
+    private void bfs(char[][] board, boolean[][] visited, int i, int j, int n, int m) {
+
+        Queue<Integer> xq = new LinkedList<>();
+        Queue<Integer> yq = new LinkedList<>();
+
+        LinkedList<Integer> xv = new LinkedList<>();
+        LinkedList<Integer> yv = new LinkedList<>();
+
+        xq.offer(i);
+        yq.offer(j);
+
+        boolean flag = false;
+
+        while(!xq.isEmpty()) {
+            int x = xq.poll();
+            int y = yq.poll();
+
+            xv.offer(x);
+            yv.offer(y);
+
+            visited[x][y] = true;
+            if(x <= 0 || y <= 0 || x >= n - 1 || y >= m - 1) flag = true;
+
+            if(x > 0 && !visited[x - 1][y] && board[x - 1][y] == 'O') {
+                xq.offer(x - 1);
+                yq.offer(y);
+            }
+            if(x < n - 1 && !visited[x + 1][y] && board[x + 1][y] == 'O') {
+                xq.offer(x + 1);
+                yq.offer(y);
+            }
+            if(y > 0 && !visited[x][y - 1] && board[x][y - 1] == 'O') {
+                xq.offer(x);
+                yq.offer(y - 1);
+            }
+            if(y < m - 1 && !visited[x][y + 1] && board[x][y + 1] == 'O') {
+                xq.offer(x);
+                yq.offer(y + 1);
+            }
+        }
+
+        if(!flag) {
+            for(int ii = 0; ii < xv.size(); ii++) {
+                board[xv.get(ii)][yv.get(ii)] = 'X';
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SurroundedRegionsMy c = new SurroundedRegionsMy();
+
+        char[][] board = {
+                {'O', 'O', 'O', 'O', 'X', 'X'},
+                {'O', 'O', 'O', 'O', 'O', 'O'},
+                {'O', 'X', 'O', 'X', 'O', 'O'},
+                {'O', 'X', 'O', 'O', 'X', 'O'},
+                {'O', 'X', 'O', 'X', 'O', 'O'},
+                {'O', 'X', 'O', 'O', 'O', 'O'},
+
+        };
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+
+        c.solve(board);
+        System.out.println();
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println(board);
+    }
+}
